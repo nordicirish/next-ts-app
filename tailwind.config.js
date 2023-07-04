712e18d544e7e633efd8e1a16f830565a0485827
Roger Graham <roger.graham@tuni.fi>
2023-07-04 12:03:55 +0300

    Global.css import tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  // content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}',"]
-  // remove unused filetypes to let tailwind faster
-  content: ["./app/**/*.{jsx,tsx}", "./components/**/*.{jsx,tsx}"],
 
+module.exports = {
+  content: ["./app/**/*.{jsx,tsx}", "./components/**/*.{jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+//  remove unused filetypes to let tailwind faster
