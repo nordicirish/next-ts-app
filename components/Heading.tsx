bee17a7bc7aa4ad6f5e7979cd5eb7c5a8e02a064
Roger Graham <roger.graham@tuni.fi>
2023-07-04 18:25:37 +0300

    Add fonts.tsx: Incorporate google fonts in build

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,3 +1,9 @@
+import { orbitron } from "@/app/fonts";
+//automatically incorporates font files in the built app
 export default function Heading({ children }) {
-  return <h1 className="font-bold pb-3 text-2xl">{children}</h1>;
+  return (
+    <h1 className={`font-bold pb-3 text-2xl ${orbitron.className}`}>
+      {children}
+    </h1>
+  );
 }
