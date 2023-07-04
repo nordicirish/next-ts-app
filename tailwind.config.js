29a42354919148031c6d2b7ce1afec67812585b6
Roger Graham <roger.graham@tuni.fi>
2023-07-04 11:39:16 +0300

    Add app, components folder path & filetypes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  // content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}',"]
+  // remove unused filetypes to let tailwind faster
+  content: ["./app/**/*.{jsx,tsx}", "./components/**/*.{jsx,tsx}"],
+
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
