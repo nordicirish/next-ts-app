7780e043539f74b021fb13648ab5c931f2b4abaf
Roger Graham <roger.graham@tuni.fi>
2023-07-05 10:11:16 +0300

    Page.tsx: import readFile node module and change default export to async function
    Review text now imported from md file

--- app/reviews/stardew-valley/page.tsx
+++ app/reviews/stardew-valley/page.tsx
@@ -1,12 +1,15 @@
+import { readFile } from "fs/promises";
 import Heading from "@/components/Heading";
 import ReviewImage from "@/components/ReviewImage";
-
-export default function StardewValleyPage() {
+// Server components can be async but not client components
+// Default next component are server components
+export default async function StardewValleyPage() {
+  const text = await readFile("./content/reviews/stardew-valley.md", "utf-8");
   return (
     <>
       <Heading>Stardew Valley</Heading>
       <ReviewImage>/images/stardew-valley.jpg</ReviewImage>
-      <p>this will be the review for Stardew Valley.</p>
+      <p>{text}</p>
     </>
   );
 }
