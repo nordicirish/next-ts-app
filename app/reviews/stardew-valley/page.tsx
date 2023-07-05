b8f21ff34cc05dfee03c1c5ec0fd63d86649c764
Roger Graham <roger.graham@tuni.fi>
2023-07-05 12:28:28 +0300

    Add data abstraction: reviews.tsx function handles review fetching

--- app/reviews/stardew-valley/page.tsx
+++ app/reviews/stardew-valley/page.tsx
@@ -1,28 +1,17 @@
-import { readFile } from "fs/promises";
-import matter from "gray-matter";
-import { marked } from "marked";
+import { getReview } from "@/lib/reviews";
 import Heading from "@/components/Heading";
 import ReviewImage from "@/components/ReviewImage";
-// Server components can be async but not client components
-// Default next component are server components
 export default async function StardewValleyPage() {
-  const text = await readFile("./content/reviews/stardew-valley.md", "utf-8");
-  // disable terminal log depreceated warnings for now
-  // extract content and data first
-  const {
-    content,
-    data: { title, date, image },
-  } = matter(text);
-  const html = marked(content, { headerIds: false, mangle: false });
+  const review = await getReview("stardew-valley");
   return (
     <>
-      <Heading>{title}</Heading>
-      <p className="italic pb-2">{date}</p>
-      <ReviewImage>{image}</ReviewImage>
+      <Heading>{review.title}</Heading>
+      <p className="italic pb-2">{review.date}</p>
+      <ReviewImage>{review.image}</ReviewImage>
       {/* tailwind className prose formats html */}
       <article
         className="max-w-screen-sm prose prose-slate"
-        dangerouslySetInnerHTML={{ __html: html }}
+        dangerouslySetInnerHTML={{ __html: review.body }}
       />
       {/* need to pass html as prop as react escapes html tags as a security feature <p>{html}</p> */}
     </>
