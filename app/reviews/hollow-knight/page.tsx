919611b9e3b25fe7911e0a4d70502e60f7b815db
Roger Graham <roger.graham@tuni.fi>
2023-07-05 12:36:57 +0300

    Update hollow-knight/page.tsx to use getReview function

--- app/reviews/hollow-knight/page.tsx
+++ app/reviews/hollow-knight/page.tsx
@@ -1,11 +1,19 @@
+import { getReview } from "@/lib/reviews";
 import Heading from "@/components/Heading";
 import ReviewImage from "@/components/ReviewImage";
-export default function HollowKnightPage() {
+export default async function StardewValleyPage() {
+  const review = await getReview("hollow-knight");
   return (
     <>
-      <Heading>Hollow Knight</Heading>
-      <ReviewImage>/images/hollow-knight.jpg</ReviewImage>
-      <p>This will be the review for Hollow Knight.</p>
+      <Heading>{review.title}</Heading>
+      <p className="italic pb-2">{review.date}</p>
+      <ReviewImage>{review.image}</ReviewImage>
+      {/* tailwind className prose formats html */}
+      <article
+        className="max-w-screen-sm prose prose-slate"
+        dangerouslySetInnerHTML={{ __html: review.body }}
+      />
+      {/* need to pass html as prop as react escapes html tags as a security feature <p>{html}</p> */}
     </>
   );
 }
