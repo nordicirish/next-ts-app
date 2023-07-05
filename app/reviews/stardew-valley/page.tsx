a654003f94193da84642064bc9908b10350f3bd8
Roger Graham <roger.graham@tuni.fi>
2023-07-05 10:38:35 +0300

    reviews/page.tsx: render html using marked library

--- app/reviews/stardew-valley/page.tsx
+++ app/reviews/stardew-valley/page.tsx
@@ -1,15 +1,19 @@
 import { readFile } from "fs/promises";
+import { marked } from "marked";
 import Heading from "@/components/Heading";
 import ReviewImage from "@/components/ReviewImage";
 // Server components can be async but not client components
 // Default next component are server components
 export default async function StardewValleyPage() {
   const text = await readFile("./content/reviews/stardew-valley.md", "utf-8");
+  // disable terminal log depreceated warnings for now
+  const html = marked(text, { headerIds: false, mangle: false });
   return (
     <>
       <Heading>Stardew Valley</Heading>
       <ReviewImage>/images/stardew-valley.jpg</ReviewImage>
-      <p>{text}</p>
+      <article dangerouslySetInnerHTML={{ __html: html }} />
+      {/* need to pass html as prop as react escapes html tags as a security feature <p>{html}</p> */}
     </>
   );
 }
